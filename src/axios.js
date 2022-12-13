6218b4d30b84fdebf4cd5d91a2bcec3cad2e0d52
Parv Sharma <pscoder10462@gmail.com>
2022-12-13 16:40:00 +0530

    backend connection update

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://ps-whatsapp-backend.herokuapp.com",
+  // baseURL: "https://ps-whatsapp-backend.herokuapp.com",
+  baseURL: "https://wssap-backend.vercel.app/",
 });
 
 export default instance;
